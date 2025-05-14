1c212ee08bdf073b915beab1811a1b5eb4119ab9
PhanDev34000 <vernierestephane@hotmail.com>
2025-05-14 18:48:30 +0200

    Initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { AccueilComponent } from './pages/accueil/accueil.component';
+import { FilmsComponent } from './pages/films/films.component';
+import { ReservationComponent } from './pages/reservation/reservation.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: AccueilComponent },
+  { path: 'films', component: FilmsComponent },
+  { path: 'reservation', component: ReservationComponent },
+  { path: 'contact', component: ContactComponent },
+];
---
+++ src/app/pages/mon-espace/mon-espace.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mon-espace',
+  imports: [],
+  templateUrl: './mon-espace.component.html',
+  styleUrl: './mon-espace.component.css'
+})
+export class MonEspaceComponent {
+
+}
